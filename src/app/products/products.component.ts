10e61a111cdab48730ac367b2eeafd0b7202884b
hamze ghanbari <infogh1000@gmail.com>
2021-10-01 16:23:10 +0330

    edit

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,5 +1,9 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { mockdata } from '../classes/mockdata';
+import { from, fromEvent, of } from 'rxjs';
+import {  concatMap, delay, filter , map, tap } from 'rxjs/operators';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { UserService } from '../services/user.service';
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
@@ -7,9 +11,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductsComponent implements OnInit {
 
-  constructor() { }
-
+  constructor(private http : HttpClient , private userservicve : UserService) { }
+ @ViewChild('name',{static : true}) input : ElementRef;
   ngOnInit(): void {
+
+    fromEvent(this.input.nativeElement,'input').pipe(
+      map(event => event as InputEvent),
+      map (data => (data.target as HTMLInputElement).value),
+      concatMap(item => this.userservicve.editname(item)),
+      tap(i => console.log(i))
+    ).subscribe();
+    // of(1,2,3,4,5,6,7,8,9).subscribe(val =>{
+    //   let body=JSON.stringify({
+    //     name : 'hamze'
+    //   });
+    //   let headers=new HttpHeaders({'Content-Type' : 'application/json; charset = UTF-8'});
+    //   let options={headers : headers};
+
+    //   this.http.patch(`https://jsonplaceholder.typicode.com/users/${val}`,body,options).subscribe(console.log);
+    // });
+  
+//  from(mockdata).pipe(
+//    filter(user => user.id === 1)
+//  ).subscribe(console.log);
+
+
   }
 
 }
--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,9 +1,22 @@
+import { HttpClient , HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-
+import {  delay } from 'rxjs/operators';
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
-  constructor() { }
+  constructor(private http : HttpClient) { }
+
+  public editname(val : any){
+    let body=JSON.stringify({
+      name : val
+     });
+    let headers=new HttpHeaders({'Content-Type' : 'application/json; charset = UTF-8'});
+    let options={headers : headers};
+
+   return  this.http.patch(`https://jsonplaceholder.typicode.com/users/1`,body,options).pipe(
+      delay(500)
+    )
+  }
 }
